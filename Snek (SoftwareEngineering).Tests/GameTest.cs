3c7ff6916637566c30f01bd04ce9c6bfc1dbe009
scrollmops <miwick@stud.mediadesign.de>
2023-03-23 09:53:51 +0100

    Tested game class
    done with testing

---
+++ Snek (SoftwareEngineering).Tests/GameTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+
+namespace Snek__SoftwareEngineering_.Tests
+{
+    public class GameTest
+    {
+        private readonly IGame _game;
+        private const int GridSize = 10;
+
+        public GameTest()
+        {
+            ISnake snake = new Snake(GridSize, Direction.UP, 3);
+            IGrid grid = new Grid(GridSize, snake.Head);
+            _game = new Game(grid, snake);
+        }
+
+        [Theory]
+        [InlineData(Direction.UP, true)]
+        [InlineData(Direction.RIGHT, true)]
+        [InlineData(Direction.DOWN, false)]
+        [InlineData(Direction.LEFT, false)]
+        public void Move_Success(Direction direction, bool success)
+        {
+            var wasSuccess = _game.Move(direction);
+
+            Assert.Equal(success, wasSuccess);
+        }
+    }
+}
